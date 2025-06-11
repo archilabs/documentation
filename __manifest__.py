2508de6f6c527f9a3479ea16b47b5ca90acc0fe0
archilabs <148535817+archilabs@users.noreply.github.com>
2025-06-11 19:10:24 +0100

    Ajout du module SEO + GTM

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Archilabs SEO & GTM',
+    'license': 'LGPL-3',
+    'category': 'Website',
+    'summary': 'Ajoute la meta description et Google Tag Manager',
+    'version': '1.0',
+    'author': 'Archilabs',
+    'depends': ['website'],
+    'data': [
+        'views/layout.xml',
+    ],
+    'installable': True,
+    'application': True
+}
