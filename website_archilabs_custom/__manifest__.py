7f53c9f9222bbef12f813a4b4bbe6d07c4026f53
archilabs <148535817+archilabs@users.noreply.github.com>
2025-06-12 14:45:25 +0100

    Update __manifest__.py

--- website_archilabs_custom/__manifest__.py
+++ website_archilabs_custom/__manifest__.py
@@ -1,11 +1,15 @@
 {
     'name': 'Website Archilabs Custom',
-    'version': '1.0',
-    'summary': 'Ajout de SEO et Google Tag Manager',
-    'author': 'Archilabs',
+    'version': '16.0.1.0.0',
     'category': 'Website',
+    'summary': 'Ajoute des balises SEO et GTM au site Archilabs',
+    'description': 'Ajout de meta description et de Google Tag Manager pour le site Archilabs.',
+    'author': 'Archilabs',
+    'license': 'LGPL-3',
     'depends': ['website'],
-    'data': ['views/layout.xml'],
+    'data': [
+        'views/layout.xml',
+    ],
     'installable': True,
-    'application': False
+    'application': False,
 }
