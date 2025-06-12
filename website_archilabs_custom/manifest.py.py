8ed9e4ca2ace1fa028727c0c0cd95b6dd9d78001
archilabs <148535817+archilabs@users.noreply.github.com>
2025-06-12 17:54:36 +0100

    update

---
+++ website_archilabs_custom/manifest.py.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Website Archilabs Custom',
+    'version': '1.0.0',
+    'category': 'Website',
+    'summary': 'Modifiche personalizzate per il sito Archilabs',
+    'depends': ['website'],
+    'data': ['views/layout.xml'],
+    'installable': True,
+    'application': False,
+
+}
